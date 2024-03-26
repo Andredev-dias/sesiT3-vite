e5f8ca6cbb7aebbfba1bedd36458f1de3550e343
Andredev-dias <andredias.curitiba@gmail.com>
2024-03-26 08:45:43 -0300

    card com props

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
 import Card from "./components/Card"
-import './App.css'
+import Foto01 from "./assets/imgs/foto1.jpg"
+import Foto02 from "./assets/imgs/foto2.jpg"
+import Foto03 from "./assets/imgs/foto3.jpg"
+
+import style from "../src/App.module.css"
 
 function App() {
 
   return (
-    <>
-      <Card/>
-      <Card/>
-      <Card/>  
-      <Card/>
-    </>
+    <section className={style.section1}>
+      <Card title="Paisagem 01" 
+            image={Foto01} 
+            text1="Texto sobre a paisagem" 
+            text2="Curitiba/PR"
+      />
+      <Card title="Paisagem 02" 
+            image={Foto02} 
+            text1="Texto sobre a paisagem 02" 
+            text2="Guaratuba/PR"
+      />
+          <Card title="Paisagem 03" 
+            image={Foto03} 
+            text1="Texto sobre a paisagem 03" 
+            text2="Colombo/PR"
+      />
+    </section>
   )
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,14 @@
-import Foto01 from "../assets/imgs/foto1.jpg"
-
-export default function Card(){
+export default function Card(props){
     return(
         <div>
-            <h2>Paisagem 01</h2>
-            <img src={Foto01} alt="Paisagem" width={200} height="auto"/>
-            <p>qualquer coisa</p>
-            <p>batata doce</p>
+            <h2>{props.title}</h2>
+            <img src={props.image}
+                 alt={props.title}
+                 width={200}
+                 height="auto"
+            />
+            <p>{props.text1}</p>
+            <p>{props.text2}</p>
         </div>
     )
 }
