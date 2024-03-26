0e16d31062a4ff670626412a43e98a8c91a70116
Andredev-dias <andredias.curitiba@gmail.com>
2024-03-26 11:31:27 -0300

    estrutura pronta

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,8 @@
+import style from "./Card.module.css"
+
 export default function Card(props){
     return(
-        <div>
+        <div className={style.cardTest}>
             <h2>{props.title}</h2>
             <img src={props.image}
                  alt={props.title}
