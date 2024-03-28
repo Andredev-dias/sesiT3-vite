22f26d049e9784f356cca0193b17e7d732b12cf8
Andredev-dias <andredias.curitiba@gmail.com>
2024-03-28 08:33:18 -0300

    subindo solucoes de calculos

--- src/Calc.jsx
+++ src/Calc.jsx
@@ -2,6 +2,7 @@ import style from "./Calc.module.css"
 import Menu from "./components/Menu"
 
 import { useEffect, useState } from "react"
+import { bind } from "leaflet"
 
 export const Calc = () => {
     const [numero1, setNumero1] = useState()
@@ -11,15 +12,26 @@ export const Calc = () => {
     const [respMulti, setRespMulti] = useState()
     const [respDiv, setRespDiv] = useState()
 
-    useEffect(()=> {
-        if(numero1 !== undefined && numero2 !== undefined){
-            setRespSoma(parseFloat(numero1) + parseFloat(numero2))
-            setRespSub(parseFloat(numero1) - parseFloat(numero2))
-            setRespMulti(parseFloat(numero1) * parseFloat(numero2))
-            setRespDiv(parseFloat(numero1) / parseFloat(numero2))
-        }
-    }, [numero1, numero2])
+    // useEffect(()=> {
+    //     if(numero1 !== undefined && numero2 !== undefined){
+    //         setRespSoma(parseFloat(numero1) + parseFloat(numero2))
+    //         setRespSub(parseFloat(numero1) - parseFloat(numero2))
+    //         setRespMulti(parseFloat(numero1) * parseFloat(numero2))
+    //         setRespDiv(parseFloat(numero1) / parseFloat(numero2))
+    //     }
+    // }, [numero1, numero2])
 
+    const handleSoma = (a,b) => setRespSoma(parseFloat(a) + parseFloat(b))
+    const handleSub = () => setRespSub(parseFloat(numero1) - parseFloat(numero2))
+    const handleMulti = () => setRespMulti(parseFloat(numero1) * parseFloat(numero2))
+    const handleDiv = () => setRespDiv(parseFloat(numero1) / parseFloat(numero2))
+
+    const handleAll = (a, b) => {
+        setRespSoma(parseFloat(a) + parseFloat(b))
+        setRespSub(parseFloat(a) - parseFloat(b))
+        setRespMulti(parseFloat(a) * parseFloat(b))
+        setRespDiv(parseFloat(a) / parseFloat(b))
+    }
 
     return(
         <>
@@ -27,37 +39,44 @@ export const Calc = () => {
             <div className={style.wrapCalc}>
                 <h1>Calculos</h1>
                 <div className={style.wrapInputs}>
+                    <label htmlFor="n1">Número 01</label>
                     <input
+                     id="n1"
                      type="text"
-                     placeholder="Insira o primeiro número"
                      className={style.inputStyle}
                      value={numero1}
                      onChange={(e) => setNumero1(e.target.value)}
                      />
-                    <input 
+                    <label htmlFor="n2">Número 02</label>
+                    <input
+                     id="n2"
                      type="text"
-                     placeholder="Insira o segundo número"
                      className={style.inputStyle}
                      value={numero2}
                      onChange={(e) => setNumero2(e.target.value)}
                      />
+                     <button onClick={() => {handleAll(numero1, numero2)}}>Calcular tudo</button>
                 </div>
-                <div>
+                <div className={style.wrapAllInputs}>
                     <div>
                         <h1>Soma</h1>
-                        {!isNaN(respSoma) && respSoma}
+                        {!isNaN(respSoma) ? respSoma : "digite um número"}
+                        <button onClick={() => handleSoma(numero1, numero2)}>SOMAR</button>
                     </div>
                     <div>
                         <h1>SUb</h1>
                         {!isNaN(respSub) && respSub}
+                        <button onClick={handleSub}>SUB</button>
                     </div>
                     <div>
                         <h1>Multi</h1>
                         {!isNaN(respMulti) && respMulti}
+                        <button onClick={handleMulti}>MULTI</button>
                     </div>
                     <div>
                         <h1>Div</h1>
                         {!isNaN(respDiv) && respDiv.toFixed(2)}
+                        <button onClick={handleDiv}>DIV</button>
                     </div>
                 </div>
             </div>
